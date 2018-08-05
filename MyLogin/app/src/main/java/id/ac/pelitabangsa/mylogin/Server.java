9152abaa3a9100f20ad96f299ce93cf8744dc8c3
sasmitoh <sasmitohrr@gmail.com>
2018-08-05 17:00:52 +0700

    v-01

---
+++ MyLogin/app/src/main/java/id/ac/pelitabangsa/mylogin/Server.java
@@ -0,0 +1,5 @@
+package id.ac.pelitabangsa.mylogin;
+
+public class Server {
+    public static final String URL = "http://192.168.43.169/login/";
+}
---
+++ MyLogin/app/src/main/java/id/ac/pelitabangsa/mylogin/util/Server.java
@@ -0,0 +1,5 @@
+package id.ac.pelitabangsa.mylogin.util;
+
+public class Server {
+    public static final String URL = "http://10.0.2.2/android/kuncoro_login/";
+}
