7ec5a7e3737865c3dfd8325b2209a745941c8f05
sasmitoh <sasmitohrr@gmail.com>
2018-08-15 13:16:08 +0700

    tes

---
+++ view/app/src/main/java/com/example/sasmitoh/view/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.sasmitoh.view;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView biorc;
+
+    private RequestQueue requestQueue;
+    private StringRequest stringRequest;
+
+    ArrayList<HashMap<String, String>> list_data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://192.168.43.169/login/getdata.php";
+
+        biorc = (RecyclerView) findViewById(R.id.biorec);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        biorc.setLayoutManager(llm);
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        list_data = new ArrayList<HashMap<String, String>>();
+
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("response ", response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("biodata");
+                    for (int a = 0; a < jsonArray.length(); a++) {
+                        JSONObject json = jsonArray.getJSONObject(a);
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put("id", json.getString("uid"));
+                        map.put("image_path", json.getString("image_path"));
+                        map.put("image_name", json.getString("image_name"));
+                        map.put("nim", json.getString("nim"));
+                        map.put("nohp", json.getString("nohp"));
+                        map.put("tanggal", json.getString("tanggal"));
+                        list_data.add(map);
+                        AdapterList adapter = new AdapterList(MainActivity.this, list_data);
+                        biorc.setAdapter(adapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+}
